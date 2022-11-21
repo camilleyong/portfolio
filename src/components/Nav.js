ed87e7177306c506007c2ef063b03612f1bdfacc
Camille Yong <camillionaire@Camilles-MacBook-Pro.local>
2022-11-20 17:27:02 -0800

    added navbar and it's components

---
+++ src/components/Nav.js
@@ -0,0 +1,54 @@
+// import './css/portfoliocontainer.css';
+import './css/nav.css';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+export default function SideBar() {
+    return (
+    <SideNav
+        onSelect={(selected) => {
+            console.log(selected);
+            }}
+        >
+        <Toggle />
+        <Nav defaultSelected="home">
+            <NavItem eventKey="home">
+                <NavIcon>
+                    <i class='fa fa-fw fa-home'></i>
+                    </NavIcon>
+                <NavText>Home</NavText>
+                </NavItem>
+                <NavItem eventKey="about">
+                <NavIcon>
+                    <i class="fa-sharp fa-solid fa-user"></i>
+                    </NavIcon>
+                <NavText>About</NavText>
+                </NavItem>
+                <NavItem eventKey="portfolio">
+                <NavIcon>
+                    <i class="fa-sharp fa-solid fa-clipboard"></i>
+                    </NavIcon>
+                <NavText>Portfolio</NavText>
+                </NavItem>
+                <NavItem eventKey="skills">
+                <NavIcon>
+                    <i class="fa-sharp fa-solid fa-wand-magic-sparkles"></i>
+                    </NavIcon>
+                <NavText>Skills</NavText>
+                </NavItem>
+                <NavItem eventKey="resume">
+                <NavIcon>
+                    <i class="fa-sharp fa-solid fa-file"></i>
+                    </NavIcon>
+                <NavText>Resume</NavText>
+                </NavItem>
+                <NavItem eventKey="contact">
+                <NavIcon>
+                    <i class="fa-sharp fa-solid fa-envelope"></i>
+                    </NavIcon>
+                <NavText>Contact</NavText>
+                </NavItem>
+        </Nav>
+    </SideNav>
+    );
+}
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,10 @@
+import Nav from './Nav';
+
+
+export default function PortfolioContainer() {
+    return (
+        <div>
+            <Nav />
+        </div>
+    );
+}
